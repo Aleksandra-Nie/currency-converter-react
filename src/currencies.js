e73aeae087127738db4cce11d49632f80f59aa8a
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2023-09-04 17:17:01 +0200

    currencies created

---
+++ src/currencies.js
@@ -0,0 +1,8 @@
+export const currencies = [
+    { name: "PLN - Złotówka polska", value: 1 },
+    { name: "USD - Dolar amerykański", value: 4.3981 },
+    { name: "EUR - Euro", value: 4.6871 },
+    { name: "GBP - Funt szterling", value: 5.2836 },
+    { name: "CHF - Frank szwajcarski", value: 4.7156 },
+    { name: "AUD - Dolar australijski", value: 2.9292 },
+];
