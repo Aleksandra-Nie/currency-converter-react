def440ca713333f6ff9c88e3cf1180dc6c5e3081
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2023-11-13 16:39:21 +0100

    custom hook - useCurrentDate created

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useCurrentDate } from "../useCurrentDate";
 import { StyledClock } from "./styled";
 
 const formatDate = {
@@ -11,17 +11,7 @@ const formatDate = {
 };
 
 const Clock = () => {
-    const [date, setDate] = useState(new Date());
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDate(new Date());
-        }, 1000);
-
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, []);
+    const date = useCurrentDate();
 
     return (
         <StyledClock>
