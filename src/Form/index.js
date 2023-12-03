56c8395a6b5faf6de3c7f674c5ec1b6d87f2f4eb
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2023-12-03 14:59:42 +0100

    commited by mistake, coming back to original state

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,49 +1,42 @@
 import { useState, useRef } from 'react';
+import { currencies } from "../currencies.js";
 import Result from "../Result";
 import RateResult from "../RateResult";
 import Clock from "../Clock";
-<<<<<<< HEAD
-import { StyledFieldset, StyledLegend, StyledLabelText, StyledInput, StyledButton, Message } from "./styled";
-import { useRatesData } from './useRatesData.js';
-=======
 import { StyledFieldset, StyledLegend, StyledLabelText, StyledInput, StyledButton } from "./styled";
 import { useRatesData } from '../useRatesData.js';
->>>>>>> 36099242aa52862f11f1db37b7b0dae220cfdfa4
 
 const Form = () => {
     const [amount, setAmount] = useState("");
-    const [currencyFrom, setCurrencyFrom] = useState("PLN");
-    const [currencyTo, setCurrencyTo] = useState("USD");
+    const [currencyFrom, setCurrencyFrom] = useState("PLN - Złotówka polska");
+    const [currencyTo, setCurrencyTo] = useState("USD - Dolar amerykański");
     const [result, setResult] = useState({});
     const [currencyRate, setCurrencyRate] = useState({});
     const inputRef = useRef(null);
     const ratesData = useRatesData();
-<<<<<<< HEAD
 
-    const currencies = ratesData.data;
-=======
->>>>>>> 36099242aa52862f11f1db37b7b0dae220cfdfa4
+    rates = ratesData.data.data
 
     const getResult = (amount, currencies, currencyFrom, currencyTo) => {
-        const currencyFromRate = currencies.find((currency) => currency.code === currencyFrom).value;
-        const currencyToRate = currencies.find((currency) => currency.code === currencyTo).value;
+        const currencyFromRate = currencies.find((currency) => currency.name === currencyFrom).value;
+        const currencyToRate = currencies.find((currency) => currency.name === currencyTo).value;
 
         setResult({
-            currencyFrom,
-            currencyTo,
+            currencyFrom: (currencyFrom).slice(0, 3),
+            currencyTo: (currencyTo).slice(0, 3),
             inputAmount: (+amount).toFixed(2),
-            resultAmount: (amount / currencyFromRate * currencyToRate).toFixed(2)
+            resultAmount: (amount * currencyFromRate / currencyToRate).toFixed(2)
         });
     };
 
     const getCurrencyRate = (currencies, currencyFrom, currencyTo) => {
-        const currencyFromRate = currencies.find((currency) => currency.code === currencyFrom).value;
-        const currencyToRate = currencies.find((currency) => currency.code === currencyTo).value;
+        const currencyFromRate = currencies.find((currency) => currency.name === currencyFrom).value;
+        const currencyToRate = currencies.find((currency) => currency.name === currencyTo).value;
 
 
         setCurrencyRate({
-            currencyFrom,
-            currencyTo,
+            currencyFrom: (currencyFrom).slice(0, 3),
+            currencyTo: (currencyTo).slice(0, 3),
             currencyRateResult: (currencyFromRate / currencyToRate).toFixed(currencyFrom === currencyTo ? 0 : 4)
         });
     };
@@ -62,8 +55,8 @@ const Form = () => {
 
     const resetForm = (event) => {
         event.preventDefault();
-        setCurrencyFrom("PLN");
-        setCurrencyTo("USD");
+        setCurrencyFrom("PLN - Złotówka polska");
+        setCurrencyTo("USD - Dolar amerykański");
         setResult("");
         setCurrencyRate("");
     };
@@ -75,98 +68,79 @@ const Form = () => {
             <Clock />
             <StyledFieldset>
                 <StyledLegend>Kalkulator walutowy</StyledLegend>
-                {ratesData.status === "loading" ? (
-                    <Message $loading>Trwa pobieranie kursu walut...⏱<br />
-                        Proszę o chwilę cierpliwości 🙂
-                    </Message>
-                ) : ratesData.status === "error" ? (
-                    <Message $failure>Przepraszamy, wystapił błąd. <br />
-                        Proszę spróbować poźniej.
-                    </Message>
-                ) : (
-                    <>
-                        <p>
-                            Pola wymagane oznaczone są *.
-                        </p>
-                        <p>
-                            <label>
-                                <StyledLabelText>Kwota*: </StyledLabelText>
-                                <StyledInput
-                                    ref={inputRef}
-                                    value={amount}
-                                    type="number"
-                                    name="amount"
-                                    step="0.01"
-                                    min="0"
-                                    placeholder="Tutaj wpisz kwotę"
-                                    required
-                                    onChange={({ target }) => setAmount(target.value)}
-                                />
-                            </label>
-                        </p>
-                        <p>
-                            <label>
-                                <StyledLabelText>Przelicz z: </StyledLabelText>
-                                <StyledInput
-                                    as="select"
-                                    onClick={currencyInput}
-                                    name="currencyFrom"
-                                    value={currencyFrom}
-                                    onChange={({ target }) => setCurrencyFrom(target.value)}
-                                >
-                                    {Object.keys(ratesData.data).map((currency) => (
-                                        <option
-                                            key={currency.code}
-                                            value={currency.code}
-                                        >
-                                            {currency.code}
-                                        </option>
-                                    ))}
-                                </StyledInput>
-                            </label>
-                        </p>
-                        <p>
-                            <label>
-                                <StyledLabelText>Przelicz na: </StyledLabelText>
-                                <StyledInput
-                                    as="select"
-                                    onClick={currencyInput}
-                                    name="currencyTo"
-                                    value={currencyTo}
-                                    onChange={({ target }) => setCurrencyTo(target.value)}
-                                >
-                                    {Object.keys(ratesData.data).map((currency) => (
-                                        <option
-                                            key={currency.code}
-                                            value={currency.code}
-                                        >
-                                            {currency.code}
-                                        </option>
-                                    ))}
-                                </StyledInput>
-                            </label>
-                        </p>
+                <p>
+                    Pola wymagane oznaczone są *.
+                </p>
+                <p>
+                    <label>
+                        <StyledLabelText>Kwota*: </StyledLabelText>
+                        <StyledInput
+                            ref={inputRef}
+                            value={amount}
+                            type="number"
+                            name="amount"
+                            step="0.01"
+                            min="0"
+                            placeholder="Tutaj wpisz kwotę"
+                            required
+                            onChange={({ target }) => setAmount(target.value)}
+                        />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <StyledLabelText>Przelicz z: </StyledLabelText>
+                        <StyledInput
+                            as="select"
+                            onClick={currencyInput}
+                            name="currencyFrom"
+                            value={currencyFrom}
+                            onChange={({ target }) => setCurrencyFrom(target.value)}
+                        >
+                            {currencies.map((currency) => (
+                                <option key={currency.name}>
+                                    {currency.name}
+                                </option>
+                            ))}
+                        </StyledInput>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <StyledLabelText>Przelicz na: </StyledLabelText>
+                        <StyledInput
+                            as="select"
+                            onClick={currencyInput}
+                            name="currencyTo"
+                            value={currencyTo}
+                            onChange={({ target }) => setCurrencyTo(target.value)}
+                        >
+                            {currencies.map((currency) => (
+                                <option key={currency.name}>
+                                    {currency.name}
+                                </option>
+                            ))}
+                        </StyledInput>
+                    </label>
+                </p>
 
-                        <RateResult
-                            currencyRate={currencyRate}
-                        />
-                        <Result
-                            result={result}
-                        />
-                        <p>
-                            <StyledButton>
-                                Przelicz
-                            </StyledButton>
-                        </p>
-                        <p>
-                            <StyledButton
-                                $reset
-                                onClick={resetForm}
-                                type="reset">Resetuj</StyledButton>
-                        </p>
-                    </>
-                )
-                }
+                <RateResult
+                    currencyRate={currencyRate}
+                />
+                <Result
+                    result={result}
+                />
+                <p>
+                    <StyledButton>
+                        Przelicz
+                    </StyledButton>
+                </p>
+                <p>
+                    <StyledButton
+                        $reset
+                        onClick={resetForm}
+                        type="reset">Resetuj</StyledButton>
+                </p>
             </StyledFieldset>
         </form >
     );
