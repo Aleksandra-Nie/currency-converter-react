ef8391b240531869fce3bb0354f87b835fd7fa3b
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2023-12-18 16:40:32 +0100

    colors and breakpoint added to theme.js

--- src/theme.js
+++ src/theme.js
@@ -4,5 +4,13 @@ export const theme = {
         secondary: "rgb(161, 198, 247)",
         pastelGreen: "#61ED7A",
         burntSienna: "#ED6166",
+        whiteLilac: "rgb(248, 247, 252)",
+        black: "#000",
+        blueLight: "rgb(208, 227, 252)",
+        gray: "rgb(128, 128, 128)",
+        red: "rgb(255, 0, 0)",
+    },
+    breakpoint: {
+        mobileMax: 768,
     },
 };
