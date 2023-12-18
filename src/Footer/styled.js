afc4b1ce7632ba0634d2fed1ca4feb565ad30c29
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2023-12-18 16:42:43 +0100

    styled components added to Footer

--- src/Footer/styled.js
+++ src/Footer/styled.js
@@ -4,4 +4,6 @@ export const StyledFooter = styled.footer`
     text-align: center;
     font-family: "Noto Sans Kannada", sans-serif;
     font-size: 12px;
+    padding: 20px;
+    font-style: italic;
 `;
