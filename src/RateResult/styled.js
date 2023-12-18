74b91ae120f22e414a04428f45702528e7987adb
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2023-12-18 16:42:21 +0100

    styled components - @media added to RateResult

--- src/RateResult/styled.js
+++ src/RateResult/styled.js
@@ -4,6 +4,10 @@ export const StyledRateResult = styled.p`
     margin-top: 40px;
     font-size: 20px;
 
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        margin-top: 20px;
+    }
+
     ${({ $exchangeRate }) => $exchangeRate && css`
         font-size: 16px;
         color: ${({ theme }) => theme.color.secondary};
